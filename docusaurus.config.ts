b7a5fcbfd8158a019f4abf4e8cc49a8316834f21
Alejo Lovallo <alejoe.lovallo@gmail.com>
2025-03-08 20:00:32 -0300

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,8 @@ const config: Config = {
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/fonder-docs",
+  baseUrl: "/fonder-docs/",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
